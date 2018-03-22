4e56659c3d469d211343b11f8799d8f7455ad261
Ersin Güvenç <eguvenc@gmail.com>
2018-03-23 01:18:40 +0300

    updates

--- src/Obullo/Mvc/Application.php
+++ src/Obullo/Mvc/Application.php
@@ -9,8 +9,8 @@ use Psr\{
 };
 use Obullo\Mvc\Dependency\Resolver;
 use Obullo\Mvc\{
-	Container\ContainerAwareTrait,
-	Container\ContainerAwareInterface
+    Container\ContainerAwareTrait,
+    Container\ContainerAwareInterface
 };
 use ReflectionClass;
 use RuntimeException;
@@ -26,141 +26,141 @@ use Obullo\Mvc\Config\LoaderInterface as Loader;
  */
 abstract class Application implements ContainerAwareInterface
 {
-	use ContainerAwareTrait;
+    use ContainerAwareTrait;
 
-	protected $env;
-	protected $response;
-	
-	abstract protected function configureConfig(Container $container);
-	abstract protected function configureContainer(Container $container);
-	abstract protected function configureRouter(Container $container, Loader $loader, Request $request);
-	
-	/**
-	 * Constructor
-	 * 
-	 * @param string $env env
-	 */
-	public function __construct(string $env)
-	{
-		$this->env = $env;
-	}
+    protected $env;
+    protected $response;
+    
+    abstract protected function configureConfig(Container $container);
+    abstract protected function configureContainer(Container $container);
+    abstract protected function configureRouter(Container $container, Loader $loader, Request $request);
+    
+    /**
+     * Constructor
+     * 
+     * @param string $env env
+     */
+    public function __construct(string $env)
+    {
+        $this->env = $env;
+    }
 
-	/**
-	 * Initialize to configurations
-	 *
-	 * @param Request $request request
-	 * 
-	 * @return void
-	 */
-	public function start(Request $request)
-	{
-		$container = $this->getContainer();
-		$this->configureConfig($container);
-		$this->configureContainer($container);
-		$this->configureRouter($container, $container->get('loader'), $request);
-	}
-	
-	/**
-	 * Returns to env
-	 * 
-	 * @return string
-	 */
-	public function getEnv() : string
-	{
-		return $this->env;
-	}
+    /**
+     * Initialize to configurations
+     *
+     * @param Request $request request
+     * 
+     * @return void
+     */
+    public function start(Request $request)
+    {
+        $container = $this->getContainer();
+        $this->configureConfig($container);
+        $this->configureContainer($container);
+        $this->configureRouter($container, $container->get('loader'), $request);
+    }
+    
+    /**
+     * Returns to env
+     * 
+     * @return string
+     */
+    public function getEnv() : string
+    {
+        return $this->env;
+    }
 
-	/**
-	 * Handle application process
-	 * 
-	 * @param  Request $request Psr Request
-	 * @param  Route   $route   RouteInterface
-	 * 
-	 * @return null|Response
-	 */
-	public function handle(Request $request, Route $route)
-	{
-		$container = $this->getContainer();
-		$container->share('request', $request);
+    /**
+     * Handle application process
+     * 
+     * @param  Request $request Psr Request
+     * @param  Route   $route   RouteInterface
+     * 
+     * @return null|Response
+     */
+    public function handle(Request $request, Route $route)
+    {
+        $container = $this->getContainer();
+        $container->share('request', $request);
 
-	    $handler = $route->getHandler();
-	    $response = null;
-	    if (is_callable($handler)) {
-	        $exp = explode('::', $handler);
-	        $class  = $exp[0];
-	        $method = $exp[1];
-	        $arguments = $route->getArguments();
-	        $resolver = new Resolver(new ReflectionClass($class));
-	        $resolver->setContainer($this->getContainer());
-	        $resolver->setArguments($arguments);
-	        $injectedParameters = $resolver->resolve($method);
-	        $controller = new $class;
-	        $controller->setContainer($container);
-	        $response = call_user_func_array(
-	        	array(
-	        		$controller,
-	        		$method
-	        	),
-	        	$injectedParameters
-	        );
-	    }
-	    return $response;
-	}
+        $handler = $route->getHandler();
+        $response = null;
+        if (is_callable($handler)) {
+            $exp = explode('::', $handler);
+            $class  = $exp[0];
+            $method = $exp[1];
+            $arguments = $route->getArguments();
+            $resolver = new Resolver(new ReflectionClass($class));
+            $resolver->setContainer($this->getContainer());
+            $resolver->setArguments($arguments);
+            $injectedParameters = $resolver->resolve($method);
+            $controller = new $class;
+            $controller->setContainer($container);
+            $response = call_user_func_array(
+                array(
+                    $controller,
+                    $method
+                ),
+                $injectedParameters
+            );
+        }
+        return $response;
+    }
 
-	/**
-	 * Emit response
-	 * 
-	 * @return void
-	 */
-	public function emit(Response $response)
-	{
-		if (headers_sent()) {
-		    throw new RuntimeException('Unable to emit response; headers already sent');
-		}
-		if (ob_get_level() > 0 && ob_get_length() > 0) {
-		    throw new RuntimeException('Output has been emitted previously; cannot emit response');
-		}
-		$this->response = $response;
-		$this->emitHeaders();
-		$this->emitBody();
-	}
+    /**
+     * Emit response
+     * 
+     * @return void
+     */
+    public function emit(Response $response)
+    {
+        if (headers_sent()) {
+            throw new RuntimeException('Unable to emit response; headers already sent');
+        }
+        if (ob_get_level() > 0 && ob_get_length() > 0) {
+            throw new RuntimeException('Output has been emitted previously; cannot emit response');
+        }
+        $this->response = $response;
+        $this->emitHeaders();
+        $this->emitBody();
+    }
 
-	/**
-	 * Emit headers
-	 *
-	 * @return void
-	 */
-	protected function emitHeaders()
-	{
-		$statusCode = $this->response->getStatusCode();
-		foreach ($this->response->getHeaders() as $header => $values) {
-		    $name  = $header;
-		    $first = $name === 'Set-Cookie' ? false : true;
-		    foreach ($values as $value) {
-		        header(sprintf(
-		            '%s: %s',
-		            $name,
-		            $value
-		        ), $first, $statusCode);
-		        $first = false;
-		    }
-		}
-	}
+    /**
+     * Emit headers
+     *
+     * @return void
+     */
+    protected function emitHeaders()
+    {
+        $statusCode = $this->response->getStatusCode();
+        foreach ($this->response->getHeaders() as $header => $values) {
+            $name  = $header;
+            $first = $name === 'Set-Cookie' ? false : true;
+            foreach ($values as $value) {
+                header(sprintf(
+                    '%s: %s',
+                    $name,
+                    $value
+                ), $first, $statusCode);
+                $first = false;
+            }
+        }
+    }
 
-	/**
-	 * Emit body
-	 * 
-	 * @return void
-	 */
-	protected function emitBody()
-	{
-		echo $this->response->getBody();
-	}
+    /**
+     * Emit body
+     * 
+     * @return void
+     */
+    protected function emitBody()
+    {
+        echo $this->response->getBody();
+    }
 
-	/**
-	 * Terminate application
-	 * 
-	 * @return void
-	 */
-	public function terminate() {}
+    /**
+     * Terminate application
+     * 
+     * @return void
+     */
+    public function terminate() {}
 }
