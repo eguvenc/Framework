6cebb401c3ef555529aed16c2bc37873c25ed0b0
Ersin Güvenç <eguvenc@gmail.com>
2018-05-29 13:20:01 +0300

    removed mongo sh

--- mongo-installer.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-
-wget http://pecl.php.net/get/mongodb-1.4.3.tgz
-tar -xzf mongodb-1.4.3.tgz
-sh -c "cd mongodb-1.4.3 && phpize && ./configure && sudo make install"
-echo "extension=mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`'
